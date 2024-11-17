e8bfeb575cbfa6748f1fee05b0dde8950b9753ae
Mariusz Furtek <mariuszfurtek@gmail.com>
2024-11-17 16:03:31 +0100

    KNN update

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sklearn.decomposition import PCA
 from sklearn.datasets import fetch_openml
 from sklearn.metrics import accuracy_score, confusion_matrix
 from scipy.stats import mode
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 import matplotlib
 matplotlib.use('Agg')
@@ -18,6 +20,9 @@ y = y.astype(int)  # Konwersja etykiety do typu całkowitego
 #PCA w celu przyspieszenia obliczeń
 pca = PCA(n_components=50, whiten=True, random_state=42)
 X_reduced = pca.fit_transform(X)
+
+#podział na zbiór treningowy i testowy
+X_train, X_test, y_train, y_test = train_test_split(X_reduced, y, test_size=0.2, random_state=42)
 
 #Trenowanie modelu Gaussian Mixture Model
 gmm = GaussianMixture(n_components=10, random_state=42)
@@ -51,3 +56,23 @@ plt.xlabel("Etykiety przewidywane")
 plt.ylabel("Etykiety prawdziwe")
 plt.savefig('confusion_matrix_gmm.png')
 
+# Trenowanie klasyfikatora k-NN
+knn = KNeighborsClassifier(n_neighbors=3)
+knn.fit(X_train, y_train)
+
+# Predykcja na zbiorze testowym
+y_pred_knn = knn.predict(X_test)
+
+# Ewaluacja klasyfikatora k-NN
+accuracy_knn = accuracy_score(y_test, y_pred_knn)
+print(f"Dokładność klasyfikacji (k-NN): {accuracy_knn * 100:.2f}%")
+
+# Wyświetlanie macierzy pomyłek dla k-NN
+conf_matrix_knn = confusion_matrix(y_test, y_pred_knn)
+plt.figure(figsize=(10, 8))
+plt.imshow(conf_matrix_knn, cmap='plasma')
+plt.title("Macierz pomyłek (k-NN)")
+plt.colorbar()
+plt.xlabel("Etykiety przewidywane")
+plt.ylabel("Prawdziwe etykiety")
+plt.savefig('confusion_matrix_knn.png')
